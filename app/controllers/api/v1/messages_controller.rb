f99a8338d7f5d1a0b7dbd5716fb2a63d357af7b4
Botanik1592 <anton.buryka@gmail.com>
2017-03-22 20:13:18 +0300

    Refactoring

--- app/controllers/api/v1/messages_controller.rb
+++ app/controllers/api/v1/messages_controller.rb
@@ -3,22 +3,21 @@ class Api::V1::MessagesController < ApplicationController
   before_action :doorkeeper_authorize!
 
   def create
-    errors = []
-    arr = params.to_unsafe_h[:messages]
+    errors = {}
 
-    arr.uniq.each do |mes|
+    params.to_unsafe_h[:messages].uniq.each_with_index do |mes, i|
       message = Message.create(mes.slice(:sender, :body, :service, :reciever))
       if message.errors.present?
-        errors << message.errors.full_messages
+        errors[i] = message.errors.full_messages
       else
         Sender.delay.send_message(message)
       end
     end
 
     if errors.empty?
-      render json: 'Accepted', status: 201
+      render json: {message: 'Ok'}, status: 201
     else
-      render json: {errors: errors}, status: 422
+      render json: {message: 'A few errors occurred', errors: errors}, status: 422
     end
   end
 end
