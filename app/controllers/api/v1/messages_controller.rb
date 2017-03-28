7a8c636a0302e336d1ce267e5be57e606a9e3beb
Botanik1592 <anton.buryka@gmail.com>
2017-03-28 13:59:31 +0300

    Statuses

--- app/controllers/api/v1/messages_controller.rb
+++ app/controllers/api/v1/messages_controller.rb
@@ -8,18 +8,18 @@ class Api::V1::MessagesController < ApplicationController
       begin
         message = Message.create(sender: mes[:sender], body: mes[:body], service: mes[:service], reciever: mes[:reciever], wn: Date.today.strftime("%U").to_i)
       rescue ActiveRecord::RecordNotUnique
-        { status: 409, message: message, errors: 'Duplicate message violates unique constraint' }
+        { status: :conflict, message: message, errors: ["There's exactly the same message already in our database"] }
       else
         if message.errors.present?
-          { status: 422, message: message, errors: message.errors.full_messages }
+          { status: :unprocessable_entity, message: message, errors: message.errors.full_messages }
         else
           Sender.delay.send_message(message)
-          { status: 201, message: message }
+          { status: :created, message: message }
         end
       end
     end
 
-    render json: totals, status: 207
+    render json: totals, status: :multi_status
 
   end
 end
